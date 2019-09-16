097b597fa3d43aea5e2c6380f6322100992ac11b
Peter Hencz <peter.hencz@gmail.com>
2019-09-16 19:29:01 +0200

    save

--- genart/sketch.js
+++ genart/sketch.js
@@ -20,16 +20,16 @@ const sketch = () => {
   const palette = ["#f18f01", "#048ba8", "#2e4057", "#99c24d", "#2f2d2e"];
   const createGrid = () => {
     const points = [];
-    const count = 115;
+    const count = 45;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        const radius = Math.abs(random.noise2D(u, v)) * 0.004;
+        const radius = Math.abs(random.noise2D(u, v)) * 0.04;
         points.push({
           color: random.pick(palette),
           radius,
-          rotation: random.noise2D(u, v) * 10,
+          rotation: random.noise2D(u, v) * 0.5,
           position: [u, v],
         });
       }
@@ -39,11 +39,11 @@ const sketch = () => {
   };
 
   // random.setSeed(512);
-  const points = createGrid().filter(() => random.value() > 0.2);
+  const points = createGrid().filter(() => random.value() > 0.5);
   const margin = 250;
 
   return ({ context, width, height }) => {
-    context.fillStyle = "#333";
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
 
     points.forEach(data => {
@@ -53,15 +53,15 @@ const sketch = () => {
       const x = lerp(margin, width - margin, u);
       const y = lerp(margin, height - margin, v);
 
-      // context.beginPath();
-      // context.arc(
-      //   x,
-      //   y,
-      //   (radius * width) / 1.2,
-      //   0,
-      //   Math.PI * random.value(),
-      //   false
-      // );
+      context.beginPath();
+      context.arc(
+        x,
+        y,
+        (radius * width) / 1.2,
+        0,
+        Math.PI * random.value(),
+        false
+      );
 
       // context.strokeStyle = color;
       // context.lineWidth = 5;
@@ -69,36 +69,10 @@ const sketch = () => {
 
       context.save();
       context.fillStyle = color;
-      context.font = `${radius * width}px "Arial"`;
+      context.font = `${radius * width}px "Helvetica"`;
       context.translate(x, y);
       context.rotate(rotation);
-      context.beginPath();
-      // context.bezierCurveTo(
-      //   radius * width,
-      //   rotation,
-      //   rotation,
-      //   140,
-      //   100,
-      //   100,
-      //   200,
-      //   1
-      // );
-      // context.lineWidth = (radius * width) / 100;
-      // context.strokeStyle = "#ddd";
-      // context.stroke();
-      context.bezierCurveTo(
-        radius,
-        40,
-        180,
-        radius * width * 10,
-        Math.cos(300 * Math.PI),
-        radius * 100,
-        200,
-        10
-      );
-      context.lineWidth = (radius * width) / 20;
-      context.strokeStyle = "#ccc";
-      context.stroke();
+      context.fillText("…•|–– ", 0, 0);
       context.restore();
     });
   };
