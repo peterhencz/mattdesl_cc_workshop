38e5125fe4b9edb7e02bf8ee2ed256b2f6c45594
Peter Hencz <peter.hencz@gmail.com>
2019-09-20 16:50:03 +0200

    save

--- genart/furtest.js
+++ genart/furtest.js
@@ -9,7 +9,7 @@ random.setSeed(random.getRandomSeed());
 
 const settings = {
   suffix: random.getSeed(),
-  dimensions: [1125, 2436],
+  dimensions: [2048, 2048],
 };
 console.log(random.getSeed());
 
@@ -18,12 +18,12 @@ const sketch = () => {
 
   const createGrid = () => {
     const points = [];
-    const count = 725;
+    const count = 65;
     for (let x = 0; x < count; x++) {
       for (let y = 0; y < count; y++) {
         const u = count <= 1 ? 0.5 : x / (count - 1);
         const v = count <= 1 ? 0.5 : y / (count - 1);
-        const radius = Math.abs(random.noise2D(u, v)) * 0.04;
+        const radius = Math.abs(random.noise2D(u, v))* 0.06;
         points.push({
           color: random.pick(palette),
           radius,
@@ -37,8 +37,8 @@ const sketch = () => {
   };
 
   // random.setSeed(512);
-  const points = createGrid().filter(() => random.value() > 0.4);
-  const margin = 10;
+  const points = createGrid().filter(() => random.value() > 0.0004);
+  const margin = 20;
 
   return ({ context, width, height }) => {
     context.fillStyle = "#333";
@@ -57,21 +57,17 @@ const sketch = () => {
       context.translate(x, y);
       context.rotate(rotation);
       context.beginPath();
-
-      context.lineWidth = (radius * width) / 10;
-      context.strokeStyle = "#ddd";
-      context.stroke();
       context.bezierCurveTo(
-        radius * 300,
+        100,
+        200,
         30,
-        10,
-        radius * width * 10,
-        Math.cos(10.33 * Math.PI),
-        radius * 10,
-        30,
-        100
+        400,
+        50,
+        60,
+        700,
+        80
       );
-      context.lineWidth = (radius * width) / 800;
+      context.lineWidth = (radius * width) / 100;
       context.strokeStyle = color;
       context.stroke();
       context.restore();
